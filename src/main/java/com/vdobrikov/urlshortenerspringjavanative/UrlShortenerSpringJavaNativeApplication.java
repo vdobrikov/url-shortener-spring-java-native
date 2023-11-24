60f147bef3e374393cb5db0460f276dea052bb83
Vladimir Dobrikov <hedin.mail@gmail.com>
2023-11-24 16:32:16 +0100

    Initial commit

---
+++ src/main/java/com/vdobrikov/urlshortenerspringjavanative/UrlShortenerSpringJavaNativeApplication.java
@@ -0,0 +1,13 @@
+package com.vdobrikov.urlshortenerspringjavanative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerSpringJavaNativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortenerSpringJavaNativeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vdobrikov/urlshortenerspringjavanative/UrlShortenerSpringJavaNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdobrikov.urlshortenerspringjavanative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerSpringJavaNativeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
